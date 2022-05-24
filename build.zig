3bfd5fd976fcf35a621b412cf18122bf64d6ea52
mtha <mtha@MacBook-Pro-de-mtha.local>
2022-05-24 10:47:31 +0200

    format

--- build.zig
+++ build.zig
@@ -1,6 +1,5 @@
 // https://gitlab.com/UltimaN3rd/hello-zig/-/blob/master/build.zig
 // https://zig.news/xq/zig-build-explained-part-1-59lf
-
 const std = @import("std");
 const path = std.os.path;
 const Builder = std.build.Builder;
@@ -9,11 +8,9 @@ pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("main", "main.zig");
     exe.setBuildMode(mode);
-
     // Link Raylib, libC needed ?
     exe.linkLibC();
     exe.linkSystemLibrary("raylib");
-
     // On osx, raylib buid can fail if we don't link framework
     // https://github.com/raysan5/raylib/wiki/Working-on-macOS
     exe.linkFramework("IOKit");
